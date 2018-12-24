bee960ea30552988739ee8fa610596cde8dae345
ligang <ligang1109@aliyun.com>
2018-12-24 18:44:49 +0800

    update log

--- src/log/test/simple_logger_test.cc
+++ src/log/test/simple_logger_test.cc
@@ -5,34 +5,42 @@
 #include "gtest/gtest.h"
 
 #include "log/base.h"
-#include "log/simple_logger.h"
 #include "log/console_writer.h"
-#include "log/buffer_writer.h"
 #include "log/file_writer.h"
+#include "log/buffer_writer.h"
+#include "log/async_writer.h"
 #include "log/simple_formater.h"
+#include "log/simple_logger.h"
+
 
 class SimpleLoggerTest : public ::testing::Test {
  protected:
   SimpleLoggerTest() {
-    std::string log_id("abcdefg");
-    std::string address("127.0.0.1:12345");
-
+    std::string          log_id("abcdefg");
+    std::string          address("127.0.0.1:12345");
     cppbox::FormaterSptr fr = std::make_shared<cppbox::SimpleFormater>(log_id, address);
-    cppbox::WriterSptr   cw = std::make_shared<cppbox::ConsoleWriter>();
-    cppbox::WriterSptr   fw = std::make_shared<cppbox::FileWriter>("/tmp/cppbox_simple_logger_test.log");
-    cppbox::WriterSptr   bw = std::make_shared<cppbox::BufferWriter>(fw);
 
+    cppbox::WriterSptr cw = std::make_shared<cppbox::ConsoleWriter>();
     clogger_ = new cppbox::SimpleLogger(cw, fr, cppbox::LogLevel::kDEBUG);
+
+    cppbox::WriterSptr fw = std::make_shared<cppbox::FileWriter>("/tmp/cppbox_simple_logger_buffer_test.log");
+    cppbox::WriterSptr bw = std::make_shared<cppbox::BufferWriter>(fw);
     flogger_ = new cppbox::SimpleLogger(bw, fr, cppbox::LogLevel::kDEBUG);
+
+    cppbox::WriterSptr afw = std::make_shared<cppbox::FileWriter>("/tmp/cppbox_simple_logger_async_test.log");
+    cppbox::WriterSptr aw  = std::make_shared<cppbox::AsyncWriter>(afw);
+    alogger_ = new cppbox::SimpleLogger(aw, fr, cppbox::LogLevel::kDEBUG);
   }
 
   ~SimpleLoggerTest() override {
     delete clogger_;
     delete flogger_;
+    delete alogger_;
   }
 
   cppbox::LoggerInterface *clogger_;
   cppbox::LoggerInterface *flogger_;
+  cppbox::LoggerInterface *alogger_;
 };
 
 void log(cppbox::LoggerInterface *logger) {
@@ -67,6 +75,7 @@ void log(cppbox::LoggerInterface *logger) {
 TEST_F(SimpleLoggerTest, Log) {
   log(clogger_);
   log(flogger_);
+  log(alogger_);
 }
 
 int main(int argc, char **argv) {
